fcf4bd6742d33f4c8fc2db294f10f4dff2c2804f
Adam Holloway <adam.holloway21@gmail.com>
2024-10-25 16:13:10 +0100

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default async function Home() {
   return (
     <main>
+      <Button>Click me</Button>
       <h1>Home page</h1>
     </main>
   );
