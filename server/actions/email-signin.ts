1a46c2c3d43aea4ca3fb564aad7273fbf850d563
Adam Holloway <adam.holloway21@gmail.com>
2024-11-08 15:16:10 +0000

    add 2fa

--- server/actions/email-signin.ts
+++ server/actions/email-signin.ts
@@ -1,28 +1,60 @@
-'use server'
+"use server";
 
-import { LoginSchema } from '@/types/login-schema';
+import { LoginSchema } from "@/types/login-schema";
 import { actionClient } from "@/lib/safe-action";
-import { db } from '../index';
-import { eq } from 'drizzle-orm';
-import { users } from '../schema';
+import { db } from "../index";
+import { eq } from "drizzle-orm";
+import { users } from "../schema";
+import { generateEmailVerificationToken } from "./tokens";
+import { sendVerificationEmail } from "./email";
+import { signIn } from "../auth";
+import { AuthError } from "next-auth";
 
 export const emailSignIn = actionClient
   .schema(LoginSchema)
-  .action(async ({ parsedInput: { email, password, code } }) => {
+  .action(async ({ parsedInput: { email, password } }) => {
+    try {
+      const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, email),
+      });
 
-    const existingUser = await db.query.users.findFirst({
-      where: eq(users.email, email)
-    });
+      if (existingUser?.email !== email) {
+        return { error: "Email not found" };
+      }
 
-    if (existingUser?.email !== email) {
-      return { error: "Email not found" }
+      // 2FA check
+      if (!existingUser.emailVerified) {
+        const verificationToken = await generateEmailVerificationToken(
+          existingUser.email
+        );
+        await sendVerificationEmail(
+          verificationToken[0].email,
+          verificationToken[0].token
+        );
+        return { success: "Confirmation email sent" };
+      }
+
+      await signIn("credentials", {
+        email,
+        password,
+        redirectTo: "/",
+      });
+
+      return { success: email };
+    } catch (error) {
+      console.log(error);
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case "CredentialsSignin":
+            return { error: "Incorrect credentials" };
+          case "AccessDenied":
+            return { error: error.message };
+          case "OAuthSignInError":
+            return { error: error.message };
+          default:
+            return { error: "Something went wrong" };
+        }
+      }
+      //throw error;
     }
-
-    // 2FA check
-    // if (!existingUser.emailVerified) {
-      
-    // }
-
-    console.log(email, password, code)
-    return { success: email }
-  })
+  });
--- server/auth.ts
+++ server/auth.ts
@@ -1,9 +1,14 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/server"
-import Google from "next-auth/providers/google"
-import GitHub from "next-auth/providers/github"
- 
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/server";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/types/login-schema";
+import { users } from "./schema";
+import { eq } from "drizzle-orm";
+import bycrypt from "bcrypt";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
   secret: process.env.AUTH_SECRET,
@@ -20,5 +25,26 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
+    Credentials({
+      authorize: async (credentials) => {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await db.query.users.findFirst({
+            where: eq(users.email, email),
+          });
+
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bycrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+
+        return null;
+      },
+    }),
   ],
-})
+});
